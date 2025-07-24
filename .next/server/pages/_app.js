a55334c71c3346247c4b454f0f0710315a0f97cb
Antonio <tgregor00@techie.com>
2025-07-24 15:41:06 +0300

    Fix: Fully functional layout and internal pages,r deploy

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,83 +1,120 @@
-"use strict";
 (() => {
 var exports = {};
 exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 35:
-/***/ ((module, exports, __webpack_require__) => {
+/***/ 699:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  config: () => (/* binding */ config),
+  "default": () => (/* binding */ next_route_loaderpage_2F_app_absolutePagePath_private_next_pages_2F_app_tsx_preferredRegion_middlewareConfig_e30_3D_),
+  getServerSideProps: () => (/* binding */ getServerSideProps),
+  getStaticPaths: () => (/* binding */ getStaticPaths),
+  getStaticProps: () => (/* binding */ getStaticProps),
+  reportWebVitals: () => (/* binding */ reportWebVitals),
+  routeModule: () => (/* binding */ routeModule),
+  unstable_getServerProps: () => (/* binding */ unstable_getServerProps),
+  unstable_getServerSideProps: () => (/* binding */ unstable_getServerSideProps),
+  unstable_getStaticParams: () => (/* binding */ unstable_getStaticParams),
+  unstable_getStaticPaths: () => (/* binding */ unstable_getStaticPaths),
+  unstable_getStaticProps: () => (/* binding */ unstable_getStaticProps)
+});
+
+// NAMESPACE OBJECT: ./pages/_app.tsx
+var _app_namespaceObject = {};
+__webpack_require__.r(_app_namespaceObject);
+__webpack_require__.d(_app_namespaceObject, {
+  "default": () => (MyApp)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-modules/pages/module.js
+var pages_module = __webpack_require__(185);
+var module_default = /*#__PURE__*/__webpack_require__.n(pages_module);
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/next-route-loader/helpers.js
+var helpers = __webpack_require__(182);
+// EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(893);
+// EXTERNAL MODULE: ./styles/globals.css
+var globals = __webpack_require__(764);
+;// CONCATENATED MODULE: ./pages/_app.tsx
 
 
-Object.defineProperty(exports, "__esModule", ({
-    value: true
-}));
-Object.defineProperty(exports, "default", ({
-    enumerable: true,
-    get: function() {
-        return App;
-    }
-}));
-const _interop_require_default = __webpack_require__(167);
-const _react = /*#__PURE__*/ _interop_require_default._(__webpack_require__(689));
-const _utils = __webpack_require__(232);
-/**
- * `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
- * This allows for keeping state between navigation, custom error handling, injecting additional data.
- */ async function appGetInitialProps(param) {
-    let { Component, ctx } = param;
-    const pageProps = await (0, _utils.loadGetInitialProps)(Component, ctx);
-    return {
-        pageProps
-    };
-}
-class App extends _react.default.Component {
-    render() {
-        const { Component, pageProps } = this.props;
-        return /*#__PURE__*/ _react.default.createElement(Component, pageProps);
-    }
-}
-(()=>{
-    App.origGetInitialProps = appGetInitialProps;
-})();
-(()=>{
-    App.getInitialProps = appGetInitialProps;
-})();
-if ((typeof exports.default === "function" || typeof exports.default === "object" && exports.default !== null) && typeof exports.default.__esModule === "undefined") {
-    Object.defineProperty(exports.default, "__esModule", {
-        value: true
+function MyApp({ Component, pageProps }) {
+    return /*#__PURE__*/ jsx_runtime.jsx(Component, {
+        ...pageProps
     });
-    Object.assign(exports.default, exports);
-    module.exports = exports.default;
-} //# sourceMappingURL=_app.js.map
+}
+
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-route-loader/index.js?page=%2F_app&absolutePagePath=private-next-pages%2F_app.tsx&preferredRegion=&middlewareConfig=e30%3D!
+
+        // Next.js Route Loader
+        
+        
+
+        // Import the userland code.
+        
+
+        // Re-export the component (should be the default export).
+        /* harmony default export */ const next_route_loaderpage_2F_app_absolutePagePath_private_next_pages_2F_app_tsx_preferredRegion_middlewareConfig_e30_3D_ = ((0,helpers/* hoist */.l)(_app_namespaceObject, "default"));
+
+        // Re-export methods.
+        const getStaticProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "getStaticProps")
+        const getStaticPaths = (0,helpers/* hoist */.l)(_app_namespaceObject, "getStaticPaths")
+        const getServerSideProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "getServerSideProps")
+        const config = (0,helpers/* hoist */.l)(_app_namespaceObject, "config")
+        const reportWebVitals = (0,helpers/* hoist */.l)(_app_namespaceObject, "reportWebVitals")
+
+        // Re-export legacy methods.
+        const unstable_getStaticProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getStaticProps")
+        const unstable_getStaticPaths = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getStaticPaths")
+        const unstable_getStaticParams = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getStaticParams")
+        const unstable_getServerProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getServerProps")
+        const unstable_getServerSideProps = (0,helpers/* hoist */.l)(_app_namespaceObject, "unstable_getServerSideProps")
+
+        // Create and export the route module that will be consumed.
+        const options = {"definition":{"kind":"PAGES","page":"/_app","pathname":"/_app","bundlePath":"","filename":""}}
+        const routeModule = new (module_default())({ ...options, userland: _app_namespaceObject })
+        
+        
+    
+
+/***/ }),
+
+/***/ 764:
+/***/ (() => {
+
 
 
 /***/ }),
 
-/***/ 232:
+/***/ 76:
 /***/ ((module) => {
 
-module.exports = require("next/dist/shared/lib/utils.js");
+"use strict";
+module.exports = require("next/dist/server/future/route-modules/route-module.js");
+
+/***/ }),
+
+/***/ 100:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/server/render.js");
 
 /***/ }),
 
 /***/ 689:
 /***/ ((module) => {
 
+"use strict";
 module.exports = require("react");
-
-/***/ }),
-
-/***/ 167:
-/***/ ((__unused_webpack_module, exports) => {
-
-
-
-exports._ = exports._interop_require_default = _interop_require_default;
-function _interop_require_default(obj) {
-    return obj && obj.__esModule ? obj : { default: obj };
-}
-
 
 /***/ })
 
@@ -88,7 +125,7 @@ function _interop_require_default(obj) {
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(35));
+var __webpack_exports__ = __webpack_require__.X(0, [812], () => (__webpack_exec__(699)));
 module.exports = __webpack_exports__;
 
 })();
